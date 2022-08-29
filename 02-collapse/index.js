8d36896ad3afc19fa01506cba6db8830b4c74112
AksanaYashchanka <AksanaYashchanka@gmail.com>
2022-08-29 22:05:28 +0300

    changes

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,12 +1,12 @@
 let coll = document.getElementsByClassName("collapsible__button");
 let i;
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+    coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let collapsible__content = this.nextElementSibling;
       if (collapsible__content.style.display === "block") {
       collapsible__content.style.display = "none";
-           } else {
+      } else {
       collapsible__content.style.display = "block";
     }
   });
