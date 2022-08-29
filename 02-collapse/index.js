68fe7e60471ff03c1ef0bd7885ff1cd6d926050b
AksanaYashchanka <AksanaYashchanka@gmail.com>
2022-08-29 22:01:21 +0300

    project commit

---
+++ 02-collapse/index.js
@@ -0,0 +1,14 @@
+let coll = document.getElementsByClassName("collapsible__button");
+let i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let collapsible__content = this.nextElementSibling;
+      if (collapsible__content.style.display === "block") {
+      collapsible__content.style.display = "none";
+           } else {
+      collapsible__content.style.display = "block";
+    }
+  });
+} 
+
